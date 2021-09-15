adb3cc9d7232eab0d3a6ec9d757a192b028a2dd5
tldkwnstn <hzeong66@gmail.com>
2021-09-15 17:47:30 +0900

    이메일 발송폼 수정

--- docs/index.js
+++ docs/index.js
@@ -77,3 +77,25 @@ function MousemoveEffect1__init() {
 }
 
 MousemoveEffect1__init();
+
+// 이메일 발송 폼 //
+function sendEmailForm(form) {
+  if ( form._replyto.value.length == 0 ) {
+    alert('이메일 주소를 입력해주세요.');
+    form._replyto.focus();
+    return;
+  }
+  
+  if ( form.message.value.length == 0 ) {
+    alert('메세지를 입력해주세요.');
+    form.message.focus();
+    return;
+  }
+  
+  form.submit();
+  
+  form._replyto.value = '';
+  form.message.value = '';
+  form.submit.innerHTML = '전송되었습니다.';
+  form.submit.disabled = true;
+}
