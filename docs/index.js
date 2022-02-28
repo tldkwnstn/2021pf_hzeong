fc6e446aeb4f84ed1e60e4f81c9c8020ad235b88
tldkwnstn <hzeong66@gmail.com>
2022-02-28 20:30:41 +0900

    포트폴리오 내용물 추가

--- docs/index.js
+++ docs/index.js
@@ -83,6 +83,7 @@ MousemoveEffect1__init();
 var swiper = new Swiper(".slides", {
   slidesPerView: 1,
   spaceBetween: 30,
+  loop:true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
